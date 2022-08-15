72c1a61a29e13ace35de2a00ae796020a018f50f
ELIUS <eliuswanyoro@gmail.com>
2022-08-15 16:30:42 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
-}
+function scuberGreetingForFeet(feets){
+    if (feets <= 400) {
+      return 'This one is on me!'
+  }
+  else if(feets > 2000  && feets < 2500){
+    return 'I will gladly take your thirty bucks.'
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+  }
+  else {
+    return 'No can do.'
+  }
+};
 
-function switchOnCharmFromTip(){
+function ternaryCheckCity(city){
   // Write your code here!
+  return city == 'NYC' ?'Ok, sounds good.' : 'No go.'
+     
+};
+
+function switchOnCharmFromTip(tip){
+  // Write your code here!
+   switch (tip) {
+    case 'generous':
+      return "Thank you so much."
+      
+        case 'not as generous':
+       return "Thank you."
+
+       default:
+     return   "Bye."
+   }
+    
+
 }
